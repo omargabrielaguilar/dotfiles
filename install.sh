e70b454dd3e85b3b616032d521f6a4ea616c805c
Omar <saucyfunnel@gmail.com>
2025-01-23 19:15:15 -0500

    conf neovim

--- install.sh
+++ install.sh
@@ -7,3 +7,13 @@ ln -s $DOTFILES/alacritty $HOME/.config/alacritty
 
 rm -rf $HOME/.tmux.conf
 ln -s $DOTFILES/tmux/tmux.conf $HOME/.tmux.conf
+
+mkdir -p $HOME/.local/bin
+
+rm -rf $HOME/.local/bin/t.sh
+ln -s $DOTFILES/scripts/t.sh $HOME/.local/bin/t.sh
+
+rm -rf $HOME/.config/nvim
+ln -s $DOTFILES/nvim $HOME/.config/nvim
+
+
