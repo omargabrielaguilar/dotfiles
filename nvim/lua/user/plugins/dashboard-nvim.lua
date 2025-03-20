fc5ca40be37ed041d4459bd829da8a1643416966
Omar <omareegab@gmail.com>
2025-03-20 15:30:21 -0500

    colorscheme and update

--- nvim/lua/user/plugins/dashboard-nvim.lua
+++ nvim/lua/user/plugins/dashboard-nvim.lua
@@ -1,27 +1,59 @@
-require('dashboard').setup({
-  theme = 'doom',
-  config = {
-    header = {
-      '',
-      '',
-  '        ▄██████████████▄▐█▄▄▄▄█▌',
-  '        ██████▌▄▌▄▐▐▌███▌▀▀██▀▀',
-  '        ████▄█▌▄▌▄▐▐▌▀███▄▄█▌',
-  '        ▄▄▄▄▄██████████████▀',
-  '',
-    },
-   center = {
-      { action = "enew", desc = "New file", icon = " ", key = "n" },
-      { action = "Telescope find_files", desc = "Find file", icon = " ", key = "SPC f" },
-      { action = "Telescope oldfiles", desc = "Recent files", icon = " ", key = "SPC h" },
-      { action = "Telescope live_grep", desc = "Find Word", icon = " ", key = "SPC g" },
-    },
-    footer = { '' },
-  }
+require("dashboard").setup({
+	theme = "doom",
+	config = {
+		header = {
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"       ███╗   ███╗███████╗███╗   ███╗███████╗███╗   ██╗████████╗ ██████╗        ",
+			"       ████╗ ████║██╔════╝████╗ ████║██╔════╝████╗  ██║╚══██╔══╝██╔═══██╗       ",
+			"       ██╔████╔██║█████╗  ██╔████╔██║█████╗  ██╔██╗ ██║   ██║   ██║   ██║       ",
+			"       ██║╚██╔╝██║██╔══╝  ██║╚██╔╝██║██╔══╝  ██║╚██╗██║   ██║   ██║   ██║       ",
+			"       ██║ ╚═╝ ██║███████╗██║ ╚═╝ ██║███████╗██║ ╚████║   ██║   ╚██████╔╝       ",
+			"       ╚═╝     ╚═╝╚══════╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═══╝   ╚═╝    ╚═════╝        ",
+			"",
+		},
+		center = {
+			{ action = "enew", desc = "New file", icon = " ", key = "n" },
+			{ action = "Telescope find_files", desc = "Find file", icon = " ", key = "SPC f" },
+			{ action = "Telescope oldfiles", desc = "Recent files", icon = " ", key = "SPC h" },
+			{ action = "Telescope live_grep", desc = "Find Word", icon = " ", key = "SPC g" },
+		},
+		footer = { "" },
+	},
 })
 
+-- Ajuste manual del padding para centrar verticalmente
+vim.api.nvim_exec(
+	[[
+  augroup DashboardCenter
+    autocmd!
+    autocmd VimEnter * execute "normal! zt"
+  augroup END
+]],
+	false
+)
+
 -- Colores personalizados
-vim.api.nvim_set_hl(0, 'DashboardHeader', { fg = '#6272a4' })
-vim.api.nvim_set_hl(0, 'DashboardCenter', { fg = '#f8f8f2' })
-vim.api.nvim_set_hl(0, 'DashboardKey', { fg = '#bd93f9' })
-vim.api.nvim_set_hl(0, 'DashboardFooter', { fg = '#6272a4' })
+vim.api.nvim_set_hl(0, "DashboardHeader", { fg = "#ff5555" }) -- Rojo para resaltar
+vim.api.nvim_set_hl(0, "DashboardCenter", { fg = "#f8f8f2" })
+vim.api.nvim_set_hl(0, "DashboardKey", { fg = "#bd93f9" })
+vim.api.nvim_set_hl(0, "DashboardFooter", { fg = "#6272a4" })
