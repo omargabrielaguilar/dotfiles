e0ea6467d452b83f1f93dfe04ec06b0dbafe99ef
Omar <omareegab@gmail.com>
2025-08-09 19:50:09 -0500

    setup

--- nvim/lua/biignoisee/plugins/lsp/lspconfig.lua
+++ nvim/lua/biignoisee/plugins/lsp/lspconfig.lua
@@ -6,59 +6,97 @@ return {
 		{ "antosha417/nvim-lsp-file-operations", config = true },
 	},
 	config = function()
+		local lspconfig = require("lspconfig")
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+		local capabilities = cmp_nvim_lsp.default_capabilities()
+
 		-- Keymaps al conectar LSP
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 			callback = function(ev)
 				local opts = { buffer = ev.buf, silent = true }
 
-				-- 📌 Navegación
-				opts.desc = "Show LSP references"
-				vim.keymap.set("n", "<leader>lr", "<cmd>Telescope lsp_references<CR>", opts)
-				opts.desc = "Go to declaration"
-				vim.keymap.set("n", "<leader>lD", vim.lsp.buf.declaration, opts)
-				opts.desc = "Show LSP definitions"
-				vim.keymap.set("n", "<leader>ld", "<cmd>Telescope lsp_definitions<CR>", opts)
-				opts.desc = "Show LSP implementations"
-				vim.keymap.set("n", "<leader>li", "<cmd>Telescope lsp_implementations<CR>", opts)
-				opts.desc = "Show LSP type definitions"
-				vim.keymap.set("n", "<leader>lt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+				-- 📌 Navegación LSP
+				local keymap = vim.keymap.set
+				keymap(
+					"n",
+					"<leader>lr",
+					"<cmd>Telescope lsp_references<CR>",
+					{ desc = "LSP References", buffer = ev.buf }
+				)
+				keymap(
+					"n",
+					"<leader>ld",
+					"<cmd>Telescope lsp_definitions<CR>",
+					{ desc = "LSP Definitions", buffer = ev.buf }
+				)
+				keymap("n", "<leader>lD", vim.lsp.buf.declaration, { desc = "Go to Declaration", buffer = ev.buf })
+				keymap(
+					"n",
+					"<leader>li",
+					"<cmd>Telescope lsp_implementations<CR>",
+					{ desc = "LSP Implementations", buffer = ev.buf }
+				)
+				keymap(
+					"n",
+					"<leader>lt",
+					"<cmd>Telescope lsp_type_definitions<CR>",
+					{ desc = "LSP Type Definitions", buffer = ev.buf }
+				)
 
 				-- 📌 Refactor / Acciones
-				opts.desc = "See available code actions"
-				vim.keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, opts)
-				opts.desc = "Smart rename"
-				vim.keymap.set("n", "<leader>ln", vim.lsp.buf.rename, opts)
+				keymap({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, { desc = "Code Actions", buffer = ev.buf })
+				keymap("n", "<leader>ln", vim.lsp.buf.rename, { desc = "Smart Rename", buffer = ev.buf })
 
 				-- 📌 Diagnósticos
-				opts.desc = "Show buffer diagnostics"
-				vim.keymap.set("n", "<leader>lDg", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
-				opts.desc = "Show line diagnostics"
-				vim.keymap.set("n", "<leader>ldg", vim.diagnostic.open_float, opts)
+				keymap(
+					"n",
+					"<leader>lDg",
+					"<cmd>Telescope diagnostics bufnr=0<CR>",
+					{ desc = "Buffer Diagnostics", buffer = ev.buf }
+				)
+				keymap("n", "<leader>ldg", vim.diagnostic.open_float, { desc = "Line Diagnostics", buffer = ev.buf })
 
-				-- 📌 Docs y ayuda
-				opts.desc = "Hover documentation"
-				vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover, opts)
-				opts.desc = "Signature help"
-				vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+				-- 📌 Docs
+				keymap("n", "<leader>lh", vim.lsp.buf.hover, { desc = "Hover Documentation", buffer = ev.buf })
+				keymap("i", "<C-h>", vim.lsp.buf.signature_help, { desc = "Signature Help", buffer = ev.buf })
 
-				-- 📌 Extra útil
-				opts.desc = "Format file"
-				vim.keymap.set("n", "<leader>lf", function()
+				-- 📌 Formatting
+				keymap("n", "<leader>lf", function()
 					vim.lsp.buf.format({ async = true })
-				end, opts)
-				opts.desc = "Restart LSP"
-				vim.keymap.set("n", "<leader>llr", ":LspRestart<CR>", opts)
+				end, { desc = "Format File", buffer = ev.buf })
 
-				-- 📌 Phpactor-specific extras
-				opts.desc = "Phpactor: Import Class"
-				vim.keymap.set("n", "<leader>lp", ":PhpactorImportClass<CR>", opts)
-				opts.desc = "Phpactor: Class New"
-				vim.keymap.set("n", "<leader>lpc", ":PhpactorClassNew<CR>", opts)
-				opts.desc = "Phpactor: Transform"
-				vim.keymap.set("n", "<leader>lpt", ":PhpactorTransform<CR>", opts)
-				opts.desc = "Phpactor: Context Menu"
-				vim.keymap.set("n", "<leader>lpm", ":PhpactorContextMenu<CR>", opts)
+				-- 📌 Restart LSP
+				keymap("n", "<leader>llr", ":LspRestart<CR>", { desc = "Restart LSP", buffer = ev.buf })
+
+				-- 📌 Phpactor: Refactor y utilidades
+				keymap(
+					"n",
+					"<leader>lp",
+					":PhpactorImportClass<CR>",
+					{ desc = "Phpactor: Import Class", buffer = ev.buf }
+				)
+				keymap("n", "<leader>lpc", ":PhpactorClassNew<CR>", { desc = "Phpactor: Class New", buffer = ev.buf })
+				keymap("n", "<leader>lpt", ":PhpactorTransform<CR>", { desc = "Phpactor: Transform", buffer = ev.buf })
+				keymap(
+					"n",
+					"<leader>lpm",
+					":PhpactorContextMenu<CR>",
+					{ desc = "Phpactor: Context Menu", buffer = ev.buf }
+				)
+				keymap(
+					"n",
+					"<leader>lpo",
+					":PhpactorGotoDefinition<CR>",
+					{ desc = "Phpactor: Goto Definition", buffer = ev.buf }
+				)
+				keymap(
+					"n",
+					"<leader>lpr",
+					":PhpactorRenameVariable<CR>",
+					{ desc = "Phpactor: Rename Variable", buffer = ev.buf }
+				)
+				keymap("n", "<leader>lpp", ":PhpactorCopyClass<CR>", { desc = "Phpactor: Copy Class", buffer = ev.buf })
 			end,
 		})
 
@@ -75,10 +113,6 @@ return {
 			underline = true,
 			update_in_insert = false,
 		})
-
-		local lspconfig = require("lspconfig")
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
-		local capabilities = cmp_nvim_lsp.default_capabilities()
 
 		-- Lua LSP
 		lspconfig.lua_ls.setup({
@@ -97,16 +131,22 @@ return {
 			},
 		})
 
-		-- PHP con Phpactor
+		-- PHP con Phpactor (full power)
 		lspconfig.phpactor.setup({
 			capabilities = capabilities,
 			filetypes = { "php" },
 			root_dir = function(fname)
-				return lspconfig.util.root_pattern("composer.json", ".git")(fname) or vim.fn.getcwd()
+				return lspconfig.util.root_pattern("composer.json", ".git", ".phpactor.json")(fname) or vim.fn.getcwd()
 			end,
 			init_options = {
-				["language_server_phpstan.enabled"] = true, -- si tienes phpstan instalado
-				["language_server_psalm.enabled"] = false,
+				["language_server_phpstan.enabled"] = true, -- Si tienes PHPStan
+				["language_server_psalm.enabled"] = false, -- O ponlo en true si prefieres Psalm
+				["code_transform.refactor.extract_constant"] = true,
+				["code_transform.refactor.extract_method"] = true,
+				["code_transform.refactor.extract_class"] = true,
+				["language_server_completion.trim_leading_dollar"] = true,
+				["language_server.diagnostics_on_save"] = true,
+				["language_server_phpcsfixer.enabled"] = true,
 			},
 		})
 	end,
