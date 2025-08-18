57c00fcf04443ab6662dd9415bd81964b671a5b6
Omar <omareegab@gmail.com>
2025-08-18 07:59:39 -0500

    luarc
    new config

--- nvim/lua/lsp/lua.lua
+++ nvim/lua/lsp/lua.lua
@@ -1,45 +1,12 @@
 return function(lspconfig, capabilities)
-    lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-        settings = {
-            Lua = {
-                runtime = {
-                    version = "LuaJIT",
-                },
-                diagnostics = {
-                    globals = { "vim" },
-                },
-                completion = {
-                    callSnippet = "Replace",
-                },
-                workspace = {
-                    library = {
-                        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                        [vim.fn.expand("$VIMRUNTIME/lua/vim")] = true,
-                        [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-                        [vim.fn.stdpath("config") .. "/lua"] = true,
-                    },
-                    maxPreload = 10000,
-                    preloadFileSize = 10000,
-                    checkThirdParty = false,
-                },
-                telemetry = {
-                    enable = false,
-                },
-                format = {
-                    enable = true,
-                    -- Puedes configurar el estilo de formateo aquí
-                    defaultConfig = {
-                        indent_style = "space",
-                        indent_size = "2",
-                        quote_style = "single",
-                    }
-                }
-            },
-        },
-        on_attach = function(client, bufnr)
-            -- Asegurarse de que el formateo esté habilitado para este cliente
-            client.server_capabilities.documentFormattingProvider = true
-        end
-    })
+	lspconfig.lua_ls.setup({
+		capabilities = capabilities,
+		settings = {
+			Lua = {
+				runtime = { version = "LuaJIT" },
+				diagnostics = { globals = { "vim" } },
+				workspace = { checkThirdParty = false },
+			},
+		},
+	})
 end
