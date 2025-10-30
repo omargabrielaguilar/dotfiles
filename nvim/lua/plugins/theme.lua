370ce9e5425b81c7d8f16f189d7a88e943850b06
Omar <omareegab@gmail.com>
2025-10-30 18:31:25 -0500

    y no es que yo crea en la suerte

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,10 +1,18 @@
 return {
-  "oxfist/night-owl.nvim",
-  lazy = false,         -- carga inmediatamente
-  priority = 1000,      -- asegúrate que sea el primero
-  config = function()
-    require("night-owl").setup()
-    vim.cmd.colorscheme("night-owl")
-  end,
-}
+	"oxfist/night-owl.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("night-owl").setup()
+		vim.cmd.colorscheme("night-owl")
 
+		-- 💫 Forzar transparencia (funciona en TODOS los temas)
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+		vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+		vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+		vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+	end,
+}
