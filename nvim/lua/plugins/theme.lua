d82af08c802a7ac671639d12aa05c90cfc4ced7d
Omar <omareegab@gmail.com>
2026-01-26 08:49:32 -0500

    cambios en autocmd

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,31 +1,37 @@
 return {
-	"navarasu/onedark.nvim",
+	"ellisonleao/gruvbox.nvim",
 	lazy = false,
-	priority = 1001,
+	priority = 1000,
 	config = function()
-		require("onedark").setup({
-			style = "dark", -- dark > darker para contrast clean
-			transparent = true, -- CLAVE
-			term_colors = true,
+		require("gruvbox").setup({
+			terminal_colors = true,
+			undercurl = true,
+			underline = true,
+			bold = true,
 
-			code_style = {
-				comments = "italic",
-				keywords = "none",
-				functions = "none",
-				strings = "none",
-				variables = "none",
+			italic = {
+				strings = true,
+				emphasis = true,
+				comments = true,
+				operators = false,
+				folds = true,
 			},
 
-			diagnostics = {
-				darker = true,
-				undercurl = true,
-				background = false,
-			},
+			strikethrough = true,
+			invert_selection = false,
+			invert_signs = false,
+			invert_tabline = false,
+			inverse = true,
+
+			contrast = "hard", -- CLAVE: gruvbox hard
+			dim_inactive = false,
+			transparent_mode = true, -- CLAVE: transparencia nativa
 		})
 
-		vim.cmd.colorscheme("onedark")
+		-- MUY IMPORTANTE: después del setup
+		vim.cmd.colorscheme("gruvbox")
 
-		-- 🔎 Fuerza background NONE (por si algún plugin se pone pesado)
+		-- 🔎 Fuerza transparencia (anti plugins)
 		local transparent_groups = {
 			"Normal",
 			"NormalNC",
@@ -33,13 +39,16 @@ return {
 			"FloatBorder",
 			"SignColumn",
 			"LineNr",
-			"CursorLine",
 			"CursorLineNr",
 			"MsgArea",
 			"StatusLine",
 			"StatusLineNC",
+
+			-- Telescope
 			"TelescopeNormal",
 			"TelescopeBorder",
+
+			-- File trees
 			"NvimTreeNormal",
 			"NvimTreeNormalNC",
 		}
