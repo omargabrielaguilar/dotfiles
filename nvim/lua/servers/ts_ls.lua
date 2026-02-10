7af9f484f15324a167362335fabfa9411bd149d2
Omar <omareegab@gmail.com>
2026-02-10 08:29:48 -0500

    ts_ls

--- nvim/lua/servers/ts_ls.lua
+++ nvim/lua/servers/ts_ls.lua
@@ -1,35 +1,77 @@
 -- ================================================================================================
--- TITLE : ts_ls (TypeScript Language Server) LSP Setup
+-- TITLE : ts_ls (TypeScript Language Server) LSP Setup - VUE & TS HYBRID MODE 🚀
 -- LINKS :
---   > github: https://github.com/typescript-language-server/typescript-language-server
+--   > https://github.com/typescript-language-server/typescript-language-server
+--   > https://github.com/vuejs/language-tools
 -- ================================================================================================
 
---- @param capabilities table LSP client capabilities (typically from nvim-cmp or similar)
+--- @param capabilities table LSP client capabilities
 --- @return nil
 return function(capabilities)
+	-- Localizamos el plugin de Volar (debe estar instalado via Mason)
+	-- Esto permite que TS entienda qué hay dentro de los bloques <script> de Vue
+	local vue_plugin_path = vim.fn.expand(
+		"$HOME/.local/share/nvim/mason/packages/vue-language-server/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin"
+	)
+
 	vim.lsp.config("ts_ls", {
 		capabilities = capabilities,
+
+		-- Añadimos "vue" a los filetypes para que el servidor se active en ellos
 		filetypes = {
 			"typescript",
 			"javascript",
 			"typescriptreact",
 			"javascriptreact",
+			"vue",
 		},
+
 		init_options = {
+			-- 🔌 PLUGIN DE VUE: La magia para que TS entienda archivos .vue
+			plugins = {
+				{
+					name = "@vue/typescript-plugin",
+					location = vue_plugin_path,
+					languages = { "javascript", "typescript", "vue" },
+				},
+			},
 			preferences = {
 				includeCompletionsForImportStatements = true,
+				includeCompletionsWithSnippetText = true,
+				includeCompletionsWithInsertText = true,
 			},
-			plugins = {},
 		},
+
 		settings = {
 			typescript = {
+				updateImportsOnFileMove = { enabled = "always" },
+				suggest = {
+					completeFunctionCalls = true,
+				},
+				inlayHints = {
+					includeInlayParameterNameHints = "all",
+					includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+					includeInlayFunctionParameterTypeHints = true,
+					includeInlayVariableTypeHints = true,
+					includeInlayPropertyDeclarationTypeHints = true,
+					includeInlayFunctionLikeReturnTypeHints = true,
+					includeInlayEnumMemberValueHints = true,
+				},
 				format = {
 					indentSize = 2,
 					convertTabsToSpaces = true,
 				},
-				preferences = {},
 			},
-			javascript = {},
+			javascript = {
+				updateImportsOnFileMove = { enabled = "always" },
+				suggest = {
+					completeFunctionCalls = true,
+				},
+				inlayHints = {
+					includeInlayParameterNameHints = "all",
+					includeInlayVariableTypeHints = true,
+				},
+			},
 		},
 	})
 end
